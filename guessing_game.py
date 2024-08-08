e69662badd99b89a7bd9fff68b65507322377ad4
Caroline Kranefuss <98840718+caroline-kranefuss@users.noreply.github.com>
2024-08-08 12:42:45 -0400

    Create guessing_game.py

---
+++ guessing_game.py
@@ -0,0 +1,41 @@
+import random
+
+def main():
+    guess(generate_number(get_level())) #so cool; can split up if big
+
+def get_level(): #doesn't take an argument here or above
+    while True:
+        try:
+            number = int(input("Level: "))
+            if number > 0:
+                return number
+            else:
+                pass
+        except ValueError:
+            pass
+
+def generate_number(number_generated):
+    return random.randint(1, number_generated)
+
+def guess(number_generated):
+    while True:
+        try:
+            guess = int(input("Guess: "))
+            if guess > 0:
+                if guess > number_generated:
+                    print("Too large!")
+                    pass
+                elif guess < number_generated:
+                    print("Too small!")
+                    pass
+                else:
+                    print("Just right!")
+                    break
+            else:
+                pass
+        except ValueError:
+            pass
+
+
+if __name__ == "__main__":
+    main()
