45b877ca4b883ad0781ef346422ca8e93e6ab20a
Caroline Kranefuss <98840718+caroline-kranefuss@users.noreply.github.com>
2024-11-08 16:15:35 -0500

    Create card_1.java

---
+++ card_1.java
@@ -0,0 +1,70 @@
+import stdlib.StdOut;
+import stdlib.StdRandom;
+
+public class Card {
+    public static void main(String[] args) {
+        // Get a random number between 2 and 14
+        int rank = StdRandom.uniform(2, 15);
+        // Set rankStr to a string depending on which int is found for rank
+        String rankStr = "nothing at the moment";
+        if (rank == 2) {
+            rankStr = "2";
+        }
+        else if (rank == 3) {
+            rankStr = "3";
+        }
+        else if (rank == 4) {
+            rankStr = "4";
+        }
+        else if (rank == 5) {
+            rankStr = "5";
+        }
+        else if (rank == 6) {
+            rankStr = "6";
+        }
+        else if (rank == 7) {
+            rankStr = "7";
+        }
+        else if (rank == 8) {
+            rankStr = "8";
+        }
+        else if (rank == 9) {
+            rankStr = "9";
+        }
+        else if (rank == 10) {
+            rankStr = "10";
+        }
+        else if (rank == 11) {
+            rankStr = "Jack";
+        }
+        else if (rank == 12) {
+            rankStr = "Queen";
+        }
+        else if (rank == 13) {
+            rankStr = "King";
+        }
+        else if (rank == 14) {
+            rankStr = "Ace";
+        }
+        // Random number between 1 and 4
+        int suit = StdRandom.uniform(1, 5);
+        // Set suitStr to a string depending on suit value
+        String suitStr = "nothing at the moment";
+        if (suit == 1) {
+            suitStr = "Clubs";
+        }
+        else if (suit == 2) {
+            suitStr = "Diamonds";
+        }
+        else if (suit == 3) {
+            suitStr = "Hearts";
+        }
+        else if (suit == 4) {
+            suitStr = "Spades";
+        }
+        else
+            StdOut.println("");
+        // Print rankStr and suitStr
+        StdOut.println(rankStr + " of " + suitStr);
+    }
+}
