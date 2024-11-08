65592912d0065a7e9023a2e0d6f25a635eb93815
Caroline Kranefuss <98840718+caroline-kranefuss@users.noreply.github.com>
2024-11-08 16:14:53 -0500

    Create die_1.java

---
+++ die_1.java
@@ -0,0 +1,38 @@
+import stdlib.StdOut;
+import stdlib.StdRandom;
+
+public class Die {
+    public static void main(String[] args) {
+        // Get a random integer using the StdRandom library
+        int value = StdRandom.uniform(1, 7);
+        String output;
+        // Depending on what the int is, print a series of stars, using \n to make a grid
+        if (value == 1) {
+            output = "     \n  *  \n     ";
+            StdOut.println(output);
+        }
+        else if (value == 2) {
+            output = "*    \n     \n    *";
+            StdOut.println(output);
+        }
+        else if (value == 3) {
+            output = "    *\n  *  \n    *";
+            StdOut.println(output);
+        }
+        else if (value == 4) {
+            output = "*   *\n     \n*   *";
+            StdOut.println(output);
+        }
+        else if (value == 5) {
+            output = "*   *\n  *  \n*   *";
+            StdOut.println(output);
+        }
+        else if (value == 6) {
+            output = "* * *\n     \n* * *";
+            StdOut.println(output);
+        }
+        else
+            StdOut.println("");
+    }
+}
+
