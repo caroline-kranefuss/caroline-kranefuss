c89b60094b6656598e8bef88f4992dec52913f36
Caroline Kranefuss <98840718+caroline-kranefuss@users.noreply.github.com>
2025-03-01 20:55:25 -0500

    Update statistics.java

--- java/using_random/statistics.java
+++ java/using_random/statistics.java
@@ -1,20 +1,23 @@
 // Accept a (int) and b (int) as commandline arguments
 // Generate three random doubles (x1,x2, and x3), each from the interval [a,b)
 // Compute their mean, variance, and standard deviation and print
+// Accept a (int) and b (int) as commandline arguments
+// Generate three random doubles (x1,x2, and x3), each from the interval [a,b)
+// Compute their mean, variance, and standard deviation and print
 
 import stdlib.StdOut;
 import stdlib.StdRandom;
 
 public class Stats {
     public static void main(String[] args) {
-        / from the two command-line arguments given, convert to doubles and name a and b
+        // from the two command-line arguments given, convert to doubles and name a and b
         int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[1]);
         // create an empty array of doubles of size three to store the random doubles we will compute
         double[] xes = new double[3];
         // use a for loop to generate three doubles
         for (int i = 0; i < 3; i ++) {
-            xes[i] = StdRandom.uniform(a,b);
+            xes[i] = StdRandom.uniform() * (b - a) + a;;
         }
         // then name the doubles generated
         double x1 = xes[0], x2 = xes[1], x3 = xes[2];
