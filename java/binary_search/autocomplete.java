e475ffb2fe986805627cab955dcc8c89bec66460
Caroline Kranefuss <98840718+caroline-kranefuss@users.noreply.github.com>
2025-03-06 15:30:50 -0500

    Update autocomplete.java

--- java/binary_search/autocomplete.java
+++ java/binary_search/autocomplete.java
@@ -80,38 +80,4 @@ public class Autocomplete {
         // return number of terms
         return last_Index - first_Index + 1;
     }
-
-    // Unit tests the data type. [DO NOT EDIT]
-    public static void main(String[] args) {
-        String filename = args[0];
-        int k = Integer.parseInt(args[1]);
-        In in = new In(filename);
-        int n = in.readInt();
-        Term[] terms = new Term[n];
-        for (int i = 0; i < n; i++) {
-            long weight = in.readLong();
-            in.readChar();
-            String query = in.readLine();
-            terms[i] = new Term(query.trim(), weight);
-        }
-        Autocomplete autocomplete = new Autocomplete(terms);
-        StdOut.print("Enter a prefix (or ctrl-d to quit): ");
-        while (StdIn.hasNextLine()) {
-            String prefix = StdIn.readLine();
-            Term[] results = autocomplete.allMatches(prefix);
-            String msg = " matches for \"" + prefix + "\", in descending order by weight:";
-            if (results.length == 0) {
-                msg = "No matches";
-            } else if (results.length > k) {
-                msg = "First " + k + msg;
-            } else {
-                msg = "All" + msg;
-            }
-            StdOut.printf("%s\n", msg);
-            for (int i = 0; i < Math.min(k, results.length); i++) {
-                StdOut.println("  " + results[i]);
-            }
-            StdOut.print("Enter a prefix (or ctrl-d to quit): ");
-        }
-    }
 }
