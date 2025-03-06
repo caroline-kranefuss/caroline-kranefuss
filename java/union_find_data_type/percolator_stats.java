0215a5875c69f99317360d750797059819d0e3be
Caroline Kranefuss <98840718+caroline-kranefuss@users.noreply.github.com>
2025-03-06 15:33:20 -0500

    Update percolator_stats.java

--- java/union_find_data_type/percolator_stats.java
+++ java/union_find_data_type/percolator_stats.java
@@ -63,15 +63,4 @@ public class PercolationStats {
     public double confidenceHigh() {
         return StdStats.mean(x) + (1.96 * StdStats.stddev(x) / Math.sqrt(m));
     }
-
-    // Unit tests the data type. 
-    public static void main(String[] args) {
-        int n = Integer.parseInt(args[0]);
-        int m = Integer.parseInt(args[1]);
-        PercolationStats stats = new PercolationStats(n, m);
-        StdOut.printf("Percolation threshold for a %d x %d system:\n", n, n);
-        StdOut.printf("  Mean                = %.3f\n", stats.mean());
-        StdOut.printf("  Standard deviation  = %.3f\n", stats.stddev());
-        StdOut.printf("  Confidence interval = [%.3f, %.3f]\n", stats.confidenceLow(), stats.confidenceHigh());
-    }
 }
